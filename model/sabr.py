309e46c61e89e0c358207d198743c6e0b9d093e6
utopian <wuthering1118@gmail.com>
2018-12-01 12:33:32 +0900

    feat: add SABR utility

--- model/sabr.py
+++ model/sabr.py
@@ -29,22 +29,38 @@ def strike_by_delta(forward, delta, tau, alpha, beta, rho, nu):
         forward, strike, sabr_implied_volatility(forward, strike, tau, alpha, beta, rho, nu), tau) - delta - (1 if delta < 0 else 0)
     return so.newton(error, x0 = fwd)
 
+def strike_dns(forward, tau, alpha, beta, rho, nu):
+    '''
+    Calculate Delta Neutral Straddle Strike
+    '''
+    error = lambda strike : bs.black_delta(
+        forward, strike, sabr_implied_volatility(forward, strike, tau, alpha, beta, rho, nu), tau) * 2 - 1
+    return so.newton(error, x0 = fwd)
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     alpha = 0.3
     beta = 0.7
-    rho = -0.2
+    rho = -0.5
     nu = 1
     fwd = 100
     tau = 1
     strikes = np.arange(0.7*fwd, 1.3*fwd)
     vols = sabr_implied_volatility(fwd, strikes, tau, alpha, beta, rho, nu)
     plt.plot(strikes, vols, label = 'sabr')
-    plt.legend()
-    plt.show()
+    # plt.legend()
+    # plt.show()
 
-    delta = -0.25
-    deltastrike = strike_by_delta(fwd, delta, tau, alpha, beta, rho, nu)
-    print('Strike :', deltastrike)
+    strike25P = strike_by_delta(fwd, -0.25, tau, alpha, beta, rho, nu)
+    strike25C = strike_by_delta(fwd, 0.25, tau, alpha, beta, rho, nu)
+    deltaFwd = bs.black_delta(
+        fwd, fwd, sabr_implied_volatility(fwd, fwd, tau, alpha, beta, rho, nu), tau)
+    strikeDns = strike_dns(fwd, tau, alpha, beta, rho, nu)
+    print('Strike25P :', strike25P)
+    print('Strike25C :', strike25C)
+    print('Fwd :', fwd)
+    print('DeltaFwd :', deltaFwd)
+    print('StrikeDns :', strikeDns)
+
