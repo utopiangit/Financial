efb753cf2aea28eb6e0df0a8e8fb686bb16c1c08
utopian <wuthering1118@gmail.com>
2018-12-01 17:05:44 +0900

    rename holee to ho_lee

--- model/ho_lee.py
+++ model/ho_lee.py
@@ -8,9 +8,9 @@ class HoLee(object):
                  volatility: float, 
                  curve_initial, 
                  t_initial: float = 0):
-        self.__volatility = volatility
-        self.__curve = curve_initial
-        self.__t_initial = t_initial
+        self.volatility = volatility
+        self.curve_initial = curve_initial
+        self.t_initial = t_initial
         
     def get_curve(self, 
                   t_observe: float, 
@@ -22,10 +22,10 @@ class HoLee(object):
     def func_a(self, 
                t_observe: float, 
                t_maturity: float) -> float:
-        initial = (self.__curve.get_df(t_maturity - self.__t_initial) 
-            / self.__curve.get_df(t_observe - self.__t_initial))
+        initial = (self.curve_initial.get_df(t_maturity - self.t_initial) 
+            / self.curve_initial.get_df(t_observe - self.t_initial))
         evolve = np.exp(-self.func_b(t_observe, t_maturity) * self.dlnPdT(t_observe)
-            - 1 / 2 * self.__volatility**2 * (t_observe - self.__t_initial) * self.func_b(t_observe, t_maturity)**2)
+            - 1 / 2 * self.volatility**2 * (t_observe - self.t_initial) * self.func_b(t_observe, t_maturity)**2)
         return initial * evolve
 
     def func_b(self, 
@@ -36,8 +36,8 @@ class HoLee(object):
     def dlnPdT(self, 
                t_observe: float) -> float:
         shock = 1/365
-        p = np.log(self.__curve.get_df(t_observe - self.__t_initial + shock))        
-        n = np.log(self.__curve.get_df(t_observe - self.__t_initial - shock))
+        p = np.log(self.curve_initial.get_df(t_observe - self.t_initial + shock))        
+        n = np.log(self.curve_initial.get_df(t_observe - self.t_initial - shock))
         return (p - n) / (2 * shock)
 
 if __name__ == '__main__':
