4d9747a0ac1c6ce548fbae084bbbaca64f87c6d4
utopian <wuthering1118@gmail.com>
2018-12-13 00:07:34 +0900

    feat: add libor

--- haskell/Swap.hs
+++ haskell/Swap.hs
@@ -17,16 +17,22 @@ backward (i, j) (r, q) = sum . map contribution $ [jmin..jmax]
         contribution k = prob i k * (exp . negate $ r!(i-1, k) * dt)
 -}
 
-backward :: Int -> Int -> Array Int Double -> Model -> [Double]
-backward from to payoff (r, q) = map (\j -> vtree!(to, j)) [jmin..jmax]
+backward :: Int -> Int -> (Int -> Double) -> Model -> (Int -> Double)
+backward from to payoff (r, q) = \j -> vtree!(to, j)
     where
         vtree :: Array (Int, Int) Double
-        vtree = listArray ((0, jmin), (nt, jmax)) 
+        vtree = listArray ((to, jmin), (from, jmax)) 
             $ map defv $ [(i, j) | i <- [to..from], j<- [jmin..jmax]]
 
-        defv (i, j) 
-            | i == from = payoff!j
+        defv (i, j) -- i == fromのときのペイオフをどう表現しよう
+            | i == from = payoff j -- 関数
+            -- | i == from = payoff!!(j - jmin) -- 配列
             | otherwise = sum . map contribution $ [jmin..jmax]
                 where 
                     contribution k = vtree!(i+1, k) 
-                        * prob j (j+1) * (exp . negate $ r!(i, k) * dt)
+                        * prob j k * (exp . negate $ r!(i, k) * dt)
+
+libor :: (Int, Int) -> Int -> Model -> Double
+libor (i, j) tenor model = (1 / df_end - 1) / (dt * fromIntegral tenor)
+    where 
+        df_end = (backward (i + tenor) i (\_ -> 1) model) j
